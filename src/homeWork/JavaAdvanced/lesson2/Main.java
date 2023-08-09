b0cdae15c6fe2e642e796c9741faac43e6cd04e9
DoggiLoL <mrspliff2283@gmail.com>
2023-08-09 10:40:28 +0300

    Add .gitignore

--- src/homeWork/JavaAdvanced/lesson2/Main.java
+++ src/homeWork/JavaAdvanced/lesson2/Main.java
@@ -2,6 +2,8 @@ package homeWork.JavaAdvanced.lesson2;
 
 public class Main {
     public static void main(String[] args) {
-        System.out.println("Hello world!");
+
+        System.out.println("Hello");
+        System.out.println("World!");
     }
 }
